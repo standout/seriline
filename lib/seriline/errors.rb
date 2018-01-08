61064ac3c3f4c3044a227f7fb4b545e5e01bd5db
Emric <w.e.w@live.se>
2018-01-08 12:40:01 +0100

    Rescue net http errors

---
+++ lib/seriline/errors.rb
@@ -0,0 +1,16 @@
+module Seriline
+  class RequestFailedError < StandardError
+    def initialize(net_http_error)
+      super build_message(net_http_error)
+    end
+
+    private
+
+    def build_message(net_http_error)
+      %(
+        Net::HTTP failed to execute request with the following status:
+        Status: #{net_http_error.code}
+      )
+    end
+  end
+end
